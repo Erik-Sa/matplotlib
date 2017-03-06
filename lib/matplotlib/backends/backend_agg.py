4a92ebe5f51dac14262e849604f845c74f256db3
Erik Sano <e.sano@mail.utoronto.ca>
2017-03-05 23:00:17 -0500

    Added the fix for Issue test_8131

--- lib/matplotlib/backends/backend_agg.py
+++ lib/matplotlib/backends/backend_agg.py
@@ -23,6 +23,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
+import warnings
 
 import threading
 import numpy as np
@@ -147,24 +148,28 @@ class RendererAgg(RendererBase):
         """
         nmax = rcParams['agg.path.chunksize'] # here at least for testing
         npts = path.vertices.shape[0]
-        if (nmax > 100 and npts > nmax and path.should_simplify and
-                rgbFace is None and gc.get_hatch() is None):
-            nch = np.ceil(npts/float(nmax))
-            chsize = int(np.ceil(npts/nch))
-            i0 = np.arange(0, npts, chsize)
-            i1 = np.zeros_like(i0)
-            i1[:-1] = i0[1:] - 1
-            i1[-1] = npts
-            for ii0, ii1 in zip(i0, i1):
-                v = path.vertices[ii0:ii1,:]
-                c = path.codes
-                if c is not None:
-                    c = c[ii0:ii1]
-                    c[0] = Path.MOVETO # move to end of last chunk
-                p = Path(v, c)
-                self._renderer.draw_path(gc, p, transform, rgbFace)
-        else:
-            self._renderer.draw_path(gc, path, transform, rgbFace)
+	try:
+            if (nmax > 100 and npts > nmax and path.should_simplify and
+                    rgbFace is None and gc.get_hatch() is None):
+                nch = np.ceil(npts/float(nmax))
+                chsize = int(np.ceil(npts/nch))
+                i0 = np.arange(0, npts, chsize)
+                i1 = np.zeros_like(i0)
+                i1[:-1] = i0[1:] - 1
+                i1[-1] = npts
+                for ii0, ii1 in zip(i0, i1):
+                    v = path.vertices[ii0:ii1,:]
+                    c = path.codes
+                    if c is not None:
+                        c = c[ii0:ii1]
+                        c[0] = Path.MOVETO # move to end of last chunk
+                    p = Path(v, c)
+                    self._renderer.draw_path(gc, p, transform, rgbFace)
+            else:
+                self._renderer.draw_path(gc, path, transform, rgbFace)
+	except OverflowError:
+	    warnings.warn('Overflow while chunking, set agg.path.chunksize'
+	        ' rcparam to excessive size')
 
     def draw_mathtext(self, gc, x, y, s, prop, angle):
         """
---
+++ test_8131.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = range(2000)
+plt.plot(x, np.sin(x))
+plt.show()
